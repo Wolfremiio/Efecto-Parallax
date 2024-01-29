00bbce058e0c446808af7bf30cfbcacaca95ad81
Wolfremiio <101276349+Wolfremiio@users.noreply.github.com>
2024-01-29 16:42:56 +0100

    Make changes in script.js and I will start with the responsive part

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
 let header = document.getElementById("header");
+
+let nube1 = document.createElement("nube1");
+let fondo_transparente = document.createElement("fondo_transparente");
+let nube2 = document.createElement("nube2");
+let btn = document.createElement("btn");
+let fondo = document.createElement("fondo");
+let viento = document.createElement("viento");
+let persona = document.createElement("persona");
+let slogan = document.createElement("slogan");
+
+window.addEventListener("scroll", function () {
+  let value = window.scrollY;
+  fondo_transparente.style.left = value * "0.7" + "px";
+  nube1.style.left = value * -5 + "px";
+  nube2.style.left = value * -5 + "px";
+  btn.style.marginTop = value * 0.7 + "px";
+  slogan.style.marginBottom = value * -1 + "px";
+  viento.style.left = value * -5 + "px";
+  header.style.marginTop = value * 0.4 + "px";
+  persona.style.marginLeft = value * 4 + "px";
+});
